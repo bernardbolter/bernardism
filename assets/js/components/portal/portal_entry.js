4724d8e41155e30268f3c75b8a8227091234951d
Bernard John Bolter IV <b@bernardbolter.com>
2016-08-06 15:21:41 -0700

    added letters react component

--- assets/js/components/portal/portal_entry.js
+++ assets/js/components/portal/portal_entry.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Component } from 'react';
 
+import PortalLetters from './portal_letters';
+
 export default class Portal extends Component {
   render() {
     return (
       <section className="portal">
-      <p>THEWEBPORTALOFBERNARDJOHNBOLTVAKATHEHISTORICALCOLORISTAKATHETRAVELERAKATHEPULLUPMANAKATHESMOOTHISTCUBIST</p>
+        <div className="portal__letters">
+          <PortalLetters text="THEWEBPORTAL" color="portal" breaks="4, 7" />
+          <span className="portal__letters--aka">OF</span>
+          <PortalLetters text="BERNARDJOHNBOLTERIV" color="bernard" breaks="8, 12, 18" />
+          <span className="portal__letters--aka">AKA</span>
+          <PortalLetters text="THEHISTORICALCOLORIST" color="history" link="http://acolorfulhistory.com/" breaks="4, 13"/>
+          <span className="portal__letters--aka">AKA</span>
+          <PortalLetters text="THETRAVELER" color="travel" link="http://www.digitalcityseries.com/" breaks="4" />
+          <span className="portal__letters--aka">AKA</span>
+          <PortalLetters text="THEPULLUPMAN" color="pullup" link="http://www.pullupgallery.org/" breaks="4, 10" />
+          <span className="portal__letters--aka">AKA</span>
+          <PortalLetters text="THESMOOTHISTCUBIST" color="smooth" link="http://www.smoothism.com/" breaks="4, 13" />
+        </div>
         <div className="portal__name-links">
           <div className="portal__name">
             <p>Bernard</p>
---
+++ assets/js/components/portal/portal_letters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class PortalLetters extends Component {
+  render() {
+    return (
+        <div className="portal__letters">
+          Portal Letters
+        </div>
+    );
+  }
+}
