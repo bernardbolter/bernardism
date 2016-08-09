751bbe7f0b4ef7e4337da9432fe6ba7af470702e
Bernard John Bolter IV <b@bernardbolter.com>
2016-08-08 23:46:53 -0700

    added interim page content and style

--- assets/js/components/artwork/artwork_entry.js
+++ assets/js/components/artwork/artwork_entry.js
@@ -9,12 +9,12 @@ export default class Portal extends Component {
           <p>the world + the work =</p>
         </div>
         <div className="artwork__wrapper">
-          <p>under construction</p>
+          <div className="artwork__art"><p>under construction</p></div>
           <div className="artwork__interim">
             <h3 className="artwork__interim--header">one old school for the interim</h3>
-            <img src="./img/alter-of-ignorance.jpg" alt="alter of ignorance - oil painting - 1997" />
-            <h2 className="artwork__interim--title">Alter of Ignorance</h2>
-            <h5 className="artwork__interim--details">24" x 14" | oil painting | 1995</h5>
+            <img src="./img/education.jpg" alt="Education - oil painting - 1997" />
+            <h2 className="artwork__interim--title">Education</h2>
+            <h5 className="artwork__interim--details">36" x 24" | oil painting | 1997</h5>
           </div>
         </div>
       </section>
