0f0a6d73e1357b653e134e615f1c426dd1fb8608
Bernard John Bolter IV <b@bernardbolter.com>
2016-08-10 00:46:55 -0700

    worked on navigation scrolling

--- assets/js/components/navigation/navigation_entry.js
+++ assets/js/components/navigation/navigation_entry.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { Component } from 'react';
 
+import classNames from 'classNames';
+
+let styles = {}
+
 export default class Portal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPressed: false
+    };
+  }
+
+  isPressed(e) {
+    e.preventDefault()
+    let bounding = this.refs.navigation.getBoundingClientRect();
+    let top = -bounding.top
+    console.log(top)
+    styles.top = top
+
+    this.setState({isPressed: true})
+  }
+
   render() {
+    var navClass = classNames({
+      'navigation': true,
+      'navigation--pressed': this.state.isPressed
+    });
     return (
-      <section className="navigation">
+      <section ref='navigation' className={navClass} style={styles}>
         <h3 className="navigation__contact">b [ at symbol ] bernardbolter.com</h3>
         <div className="navigation__links">
-          <a href="#" className="navigation__links--cv">cv</a>
-          <a href="#" className="navigation__links--bio">bio</a>
-          <a href="#" className="navigation__links--statement">statement</a>
+          <a href="#" onClick={this.isPressed.bind(this)} className="navigation__links--cv">cv</a>
+          <a href="#" onClick={this.isPressed.bind(this)} className="navigation__links--bio">bio</a>
+          <a href="#" onClick={this.isPressed.bind(this)} className="navigation__links--statement">statement</a>
           <a href="#" className="navigation__links--videos">videos</a>
         </div>
       </section>
